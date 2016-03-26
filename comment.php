b6983e0aed3412bb0c5c1ea084021ab0abe168ca
Andre Christoga <andrechristoga@gmail.com>
2016-03-26 15:16:30 +0700

    Alert message if input is not filled

--- comment.php
+++ comment.php
@@ -10,12 +10,17 @@
     // Get input value
     var nama = $('#nama').val();
     var komentar = $('#komentar').val();
+    var input = $('.form-control').val();
 
-    // Post data to firebase
-    comments.push().set({
-      nama: nama,
-      komentar: komentar
-    })
+    if (input == '') {
+      alert('Input is not filled')
+    } else {
+      // Post data to firebase
+      comments.push().set({
+        nama: nama,
+        komentar: komentar
+      })
+    }
   }
 </script>
 <script src="https://cdn.firebase.com/js/client/2.4.1/firebase.js" charset="utf-8"></script>
