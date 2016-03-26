eb1cefcd3693ae453d2ffe0baffa36552e5f59ca
Andre Christoga <andrechristoga@gmail.com>
2016-03-26 19:57:06 +0700

    Stand-alone file for meta

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
+    <?php include 'meta.php'; ?>
     <title>TauDuluan</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
