1395c32e95aa198f66a85ce6b5708ebfbd77e182
Andre Christoga <andrechristoga@gmail.com>
2016-04-04 18:03:31 +0700

    Use function rather than plain html

--- navbar.php
+++ navbar.php
@@ -1,18 +1,26 @@
+<?php
+  function navbar($link, $title){
+    echo "<li><a href=\"";
+    echo $link;
+    echo ".html\">";
+    echo $title;
+    echo "</a></li>";
+  }
+ ?>
 <nav class="navbar navbar-default" role="navigation">
   <div class="navbar-header">
     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
       <span class="sr-only">Toggle navigation</span>
     </button>
-    <a class="navbar-brand" href="">TauDuluan</a>
+    <a class="navbar-brand" href="index.php">TauDuluan</a>
   </div>
   <div class="collapse navbar-collapse" id="navbar">
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="">Home</a></li>
-      <li><a href="trending.html">Trending</a></li>
-      <li><a href="artis.html">Artis</a></li>
-      <li><a href="hotnews.html">Hot News</a></li>
-      <li><a href="populer.html">Populer</a></li>
-      <li><a href="wawancara.html">Wawancara</a></li>
+    <ul class="nav navbar-nav">      
+      <?php navbar('trending', 'Trending') ?>
+      <?php navbar('artis', 'Artis') ?>
+      <?php navbar('hotnews', 'Hotnews') ?>
+      <?php navbar('populer', 'Populer') ?>
+      <?php navbar('wawancara', 'Wawancara') ?>
     </ul>
   </div>
 </nav>
