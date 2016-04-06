77a499aba207f68915e1b95ee0accd1fa847071a
Andre Christoga <andrechristoga@gmail.com>
2016-04-07 00:21:04 +0700

    Added more sentece on headline

--- src/headline.php
+++ src/headline.php
@@ -1,7 +1,7 @@
 <div class="row">
   <div class="headline col-md-8">
     <img src="https://placeholdit.imgix.net/~text?txtsize=70&txt=Headline&w=750&h=500&txttrack=0" alt="" />
-    <h2>Headline</h2>
+    <h2>Headline TauDuluan</h2>
     <h6>
       Lorem ipsum dolor sit amet.
     </h6>
