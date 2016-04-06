3529030cf16e66279205637bf16b500380092467
Andre Christoga <andrechristoga@gmail.com>
2016-04-06 23:08:14 +0700

    Echo post title

--- src/post.php
+++ src/post.php
@@ -56,7 +56,7 @@
       </div>
       <hr>
       <div class="post">
-        <h3>Lorem ipsum dolor sit amet.</h3>
+        <h3><?php echo $post_name; ?></h3>
         <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
           labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
           nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
