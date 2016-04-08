cff8df6ea7157e2bde6805038920e7f105773c0c
Andre Christoga <andrechristoga@gmail.com>
2016-04-08 19:47:56 +0700

    Gita gutawa variable

--- src/artis.php
+++ src/artis.php
@@ -1,3 +1,4 @@
 <?php
   $artis = array('Artis', 'Lorem ipsum dolor sit amet.', 'http://placehold.it/300x150?text=Artis');
+  $gita_gutawa = array('Gita Gutawa', 'Lorem ipsum dolor sit amet.', 'http://placehold.it/300x150?text=Gita+Gutawa');
  ?>
