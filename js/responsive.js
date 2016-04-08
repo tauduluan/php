ab86cd034be1c46d2b90be0e5dc8d0befc86cc47
Andre Christoga <andrechristoga@gmail.com>
2016-04-08 09:31:31 +0700

    Cleaner code with comments

--- js/responsive.js
+++ js/responsive.js
@@ -1,9 +1,14 @@
 $(document).ready(function (){
+    // Get window width
     var width = $(window).width(),
+    // Minimized the width
         costumizedWidth = width - 30,
+    // Get image by their class
         image = $('.artis-img'),
         headline = $('.headline-img'),
         ads = $('.ads');
+
+    // Set the width css
     image.css('width', costumizedWidth);
     headline.css('width', costumizedWidth);
     ads.css('width', costumizedWidth);
