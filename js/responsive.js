fa2a3fe76194b432fe7985a58e62313b16512333
Andre Christoga <andrechristoga@gmail.com>
2016-04-08 09:25:04 +0700

    Responsive ads

--- js/responsive.js
+++ js/responsive.js
@@ -3,6 +3,8 @@ $(document).ready(function (){
         costumizedWith = width - 30,
         image = $('.artis-img');
         headline = $('.headline-img');
+        ads = $('.ads');
     image.css('width', costumizedWith);
     headline.css('width', costumizedWith);
+    ads.css('width', costumizedWith);
 });
