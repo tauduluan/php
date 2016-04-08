b306f5568b17eaae2b1289ebfb6ea47c7676c9a4
Andre Christoga <andrechristoga@gmail.com>
2016-04-08 09:25:32 +0700

    Use , rather than ;

--- js/responsive.js
+++ js/responsive.js
@@ -1,8 +1,8 @@
 $(document).ready(function (){
     var width = $(window).width(),
         costumizedWith = width - 30,
-        image = $('.artis-img');
-        headline = $('.headline-img');
+        image = $('.artis-img'),
+        headline = $('.headline-img'),
         ads = $('.ads');
     image.css('width', costumizedWith);
     headline.css('width', costumizedWith);
