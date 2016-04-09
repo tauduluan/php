f4a2391f4f0519d4dc4ef8e439eb30f05aa36158
Andre Christoga <andrechristoga@gmail.com>
2016-04-09 21:54:20 +0700

    Short article

--- gita_gutawa/post/example.php
+++ gita_gutawa/post/example.php
@@ -61,18 +61,11 @@
       <div class="post">
         <h3><?php echo $post_name; ?></h3>
         <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-          in culpa qui officia deserunt mollit anim id est laborum.
           <br><br>
           <img src="http://placehold.it/150x150?text=Caption" /><br><br>
           <span class="image-caption">Caption</span>
           <br><br>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+          Esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
           in culpa qui officia deserunt mollit anim id est laborum.
       </div>
       <div class="col-md-8">
