38bac37bf7a32be5c2ab8b835b551301f4c85f13
Andre Christoga <andrechristoga@gmail.com>
2016-04-09 21:53:29 +0700

    Testing image caption

--- gita_gutawa/post/example.php
+++ gita_gutawa/post/example.php
@@ -66,6 +66,9 @@
           esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
           in culpa qui officia deserunt mollit anim id est laborum.
           <br><br>
+          <img src="http://placehold.it/150x150?text=Caption" /><br><br>
+          <span class="image-caption">Caption</span>
+          <br><br>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
           labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
           nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
