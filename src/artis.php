3e3806243a9ef479a6c9c094c0935a5375e6cac8
Andre Christoga <andrechristoga@gmail.com>
2016-04-10 08:02:26 +0700

    Tips for the array components

--- src/artis.php
+++ src/artis.php
@@ -2,3 +2,10 @@
   $artis = array('Artis', 'Lorem ipsum dolor sit amet.', 'http://placehold.it/300x150?text=Artis');
   $gita_gutawa = array('Gita Gutawa', 'Lorem ipsum dolor sit amet.', 'http://placehold.it/300x150?text=Gita+Gutawa', 'http://placehold.it/300x150?text=Gita+Gutawa');
  ?>
+
+<!--
+  Array 1 = Name
+  Array 2 = Desc
+  Array 3 = Profile picture
+  Array 4 = Ads picture
+ -->
