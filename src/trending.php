7ec9519a86eaadd9bd6c3459a84ec9098636d008
Andre Christoga <andrechristoga@gmail.com>
2016-04-14 22:09:25 +0700

    Display the trend right now using array

---
+++ src/trending.php
@@ -0,0 +1,4 @@
+<?php
+  $title = array('1. Post satu', '2. Post dua', '3. Post tiga', '4. Post empat', '5. Post lima');
+  $desc = array('Deskripsi satu', 'Deskripsi dua', 'Deskripsi tiga', 'Deskripsi empat', 'Deskripsi lima');
+ ?>
