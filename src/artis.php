47610faf5a8a9e9ad8046e1d760bb506a10a6275
Andre Christoga <andrechristoga@gmail.com>
2016-04-14 07:09:55 +0700

    Minimize image width

--- src/artis.php
+++ src/artis.php
@@ -1,6 +1,6 @@
 <?php
   $artis = array('Artis', 'Lorem ipsum dolor sit amet.', 'http://placehold.it/300x150?text=Artis');
-  $gita_gutawa = array('Gita Gutawa', 'Lorem ipsum dolor sit amet.', 'http://placehold.it/300x150?text=Gita+Gutawa', 'http://placehold.it/300x150?text=Gita+Gutawa');
+  $gita_gutawa = array('Gita Gutawa', 'Lorem ipsum dolor sit amet.', 'http://placehold.it/270x150?text=Gita+Gutawa', 'http://placehold.it/270x150?text=Gita+Gutawa');
  ?>
 
 <!--
