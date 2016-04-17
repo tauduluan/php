7e4e6aa4b7387408098463b50c95dbffb40b0ae3
Andre Christoga <andrechristoga@gmail.com>
2016-04-17 19:50:09 +0700

    Look's like I insert too much ..

--- artis/index.php
+++ artis/index.php
@@ -1,28 +1,28 @@
-<?php include '../../src/artis.php'; ?>
+<?php include '../src/artis.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <?php include '../../src/meta.php'; ?>
+    <?php include '../src/meta.php'; ?>
     <title>TauDuluan</title>
 
     <!-- CSS -->
-    <link href="../../css/vendor/bootstrap.min.css" rel="stylesheet">
-    <link href="../../css/flat-ui.min.css" rel="stylesheet">
+    <link href="../css/vendor/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/flat-ui.min.css" rel="stylesheet">
 
     <!-- Costum -->
-    <link rel="stylesheet" href="../../css/style.css">
-    <script src="../../js/main.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../js/main.js" charset="utf-8"></script>
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
     <!--[if lt IE 9]>
-      <script src="../../js/vendor/html5shiv.js"></script>
-      <script src="../../js/vendor/respond.min.js"></script>
+      <script src="../js/vendor/html5shiv.js"></script>
+      <script src="../js/vendor/respond.min.js"></script>
     <![endif]-->
   </head>
   <body>
-    <?php include '../../src/navbar.php'; ?>
+    <?php include '../src/navbar.php'; ?>
     <div class="container">
-        <?php include '../../src/headline.php' ?>
+        <?php include '../src/headline.php' ?>
         <div class="row">
           <div class="col-md-4 ajr">
             <img src="<?php echo $gita_gutawa[2] ?>" alt="" class="artis-img"/>
@@ -43,12 +43,12 @@
             <button class="btn btn-primary" onclick="readPost('sample-post')">Read More</button>
           </div>
         </div>
-        <?php include '../../src/footer.php'; ?>
+        <?php include '../src/footer.php'; ?>
     </div>
 
     <!-- JS -->
-    <script src="../../js/vendor/jquery.min.js"></script>
-    <script src="../../js/flat-ui.min.js"></script>
-    <script src="../../js/responsive.js" charset="utf-8"></script>
+    <script src="../js/vendor/jquery.min.js"></script>
+    <script src="../js/flat-ui.min.js"></script>
+    <script src="../js/responsive.js" charset="utf-8"></script>
   </body>
 </html>
