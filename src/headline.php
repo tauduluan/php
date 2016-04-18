56e7eff6bd90d73b752e987a2c3dbf0b53ec7adc
Andre Christoga <andrechristoga@gmail.com>
2016-04-18 17:07:01 +0700

    Single column

--- src/headline.php
+++ src/headline.php
@@ -28,7 +28,7 @@ $date = '2016-4-8';
     <div class="col-md-6">
       <img src="https://placehold.it/200x200/1abc9c/bdc3c7?text=Ads" alt="Ads" class="ads"><br><br>
     </div>
-    <div class="col-md-6">
+    <!-- <div class="col-md-6">
       <img src="https://placehold.it/200x200?text=Ads" alt="Ads" class="ads"><br><br>
     </div><br>
     <div class="col-md-6">
@@ -42,7 +42,7 @@ $date = '2016-4-8';
     </div>
     <div class="col-md-6">
       <img src="https://placehold.it/200x200?text=Ads" alt="Ads" class="ads"><br><br>
-    </div>
+    </div> -->
   </div>
   <!-- YOUR ADS HERE -->
 </div>
