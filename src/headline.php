d8195a544f93e63fac7a82e68d6da71c6ce7b3b4
Andre Christoga <andrechristoga@gmail.com>
2016-04-18 23:56:35 +0700

    Remove icons from the slider navigation

--- src/headline.php
+++ src/headline.php
@@ -34,12 +34,10 @@ $date = '2016-4-8';
      </div>
 
      <!-- Left and right controls -->
-     <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-       <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+     <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">       
        <span class="sr-only">Previous</span>
      </a>
-     <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-       <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+     <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">       
        <span class="sr-only">Next</span>
      </a>
    </div>
