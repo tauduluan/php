4bb9e058274fe07bc70a37036fc9742d3e47cb16
Andre Christoga <andrechristoga@gmail.com>
2016-04-19 16:24:03 +0700

    Moved carousel slider to index

--- index.php
+++ index.php
@@ -26,6 +26,42 @@
     <?php include 'src/navbar.php'; ?>
     <div class="container">
         <?php include 'src/headline.php' ?>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#myCarousel" data-slide-to="1"></li>
+              <li data-target="#myCarousel" data-slide-to="2"></li>
+              <li data-target="#myCarousel" data-slide-to="3"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+              <div class="item active">
+                <img src="http://www.w3schools.com/bootstrap/img_chania.jpg" alt="Chania" width="460" height="345">
+              </div>
+
+              <div class="item">
+                <img src="http://www.w3schools.com/bootstrap/img_chania2.jpg" alt="Chania" width="460" height="345">
+              </div>
+
+              <div class="item">
+                <img src="http://www.w3schools.com/bootstrap/img_flower.jpg" alt="Flower" width="460" height="345">
+              </div>
+
+              <div class="item">
+                <img src="http://www.w3schools.com/bootstrap/img_flower2.jpg" alt="Flower" width="460" height="345">
+              </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
         <hr>
         <div class="row">
           <div class="col-md-4 ajr">
--- src/headline.php
+++ src/headline.php
@@ -4,43 +4,7 @@ $title = 'Headline Tauduluan';
 $gambar = 'https://placeholdit.imgix.net/~text?txtsize=70&txt=Headline&w=750&h=500&txttrack=0';
 $desc = 'Lorem ipsum dolor sit amet.';
 $date = '2016-4-8';
- ?>
- <div id="myCarousel" class="carousel slide" data-ride="carousel">
-     <!-- Indicators -->
-     <ol class="carousel-indicators">
-       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-       <li data-target="#myCarousel" data-slide-to="1"></li>
-       <li data-target="#myCarousel" data-slide-to="2"></li>
-       <li data-target="#myCarousel" data-slide-to="3"></li>
-     </ol>
-
-     <!-- Wrapper for slides -->
-     <div class="carousel-inner" role="listbox">
-       <div class="item active">
-         <img src="http://www.w3schools.com/bootstrap/img_chania.jpg" alt="Chania" width="460" height="345">
-       </div>
-
-       <div class="item">
-         <img src="http://www.w3schools.com/bootstrap/img_chania2.jpg" alt="Chania" width="460" height="345">
-       </div>
-
-       <div class="item">
-         <img src="http://www.w3schools.com/bootstrap/img_flower.jpg" alt="Flower" width="460" height="345">
-       </div>
-
-       <div class="item">
-         <img src="http://www.w3schools.com/bootstrap/img_flower2.jpg" alt="Flower" width="460" height="345">
-       </div>
-     </div>
-
-     <!-- Left and right controls -->
-     <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-       <span class="sr-only">Previous</span>
-     </a>
-     <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-       <span class="sr-only">Next</span>
-     </a>
-   </div>
+ ?> 
 <div class="row">
   <div class="headline col-md-8">
     <!-- <img src="<?php echo $gambar ?>" alt="" class="headline-img"/> -->
