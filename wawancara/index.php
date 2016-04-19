0677787c0ba0245aa1951518449a47fe14c58512
Andre Christoga <andrechristoga@gmail.com>
2016-04-19 17:13:17 +0700

    Wawancara example

---
+++ wawancara/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include '../src/meta.php'; ?>
+    <title>TauDuluan</title>
+
+    <!-- CSS -->
+    <link href="../css/vendor/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/flat-ui.min.css" rel="stylesheet">
+
+    <!-- Costum CSS -->
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../js/flat-ui.min.js"></script>
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="js/vendor/html5shiv.js"></script>
+      <script src="js/vendor/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php include '../src/navbar.php'; ?>
+    <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h3>Judul wawancara</h3>
+            <h6>Lorem ipsum dolor sit amet.</h6>
+          </div>
+          <div class="col-md-6 videos">
+            <iframe width="500" height="300" src="https://www.youtube.com/embed/jNQXAC9IVRw" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+        <?php include '../src/footer.php'; ?>
+    </div>
+
+    <!-- JS -->
+    <script src="../js/vendor/jquery.min.js"></script>
+
+  </body>
+</html>
