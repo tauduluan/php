10e605541b3c09a6a475f0d14552c818cf27f658
Andre Christoga <andrechristoga@gmail.com>
2016-04-20 17:25:25 +0700

    Basic template

---
+++ meme/index.php
@@ -0,0 +1,34 @@
+<?php include '../src/artis.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include '../src/meta.php'; ?>
+    <title>TauDuluan</title>
+
+    <!-- CSS -->
+    <link href="../css/vendor/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/flat-ui.min.css" rel="stylesheet">
+
+    <!-- Costum -->
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../js/main.js" charset="utf-8"></script>
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="../js/vendor/html5shiv.js"></script>
+      <script src="../js/vendor/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php include '../src/navbar.php'; ?>
+    <div class="container">
+
+        <?php include '../src/footer.php'; ?>
+    </div>
+
+    <!-- JS -->
+    <script src="../js/vendor/jquery.min.js"></script>
+    <script src="../js/flat-ui.min.js"></script>
+    <script src="../js/responsive.js" charset="utf-8"></script>
+  </body>
+</html>
