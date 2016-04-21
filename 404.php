3ac8048b2e88920140f79b88db89aae3b3313dd3
Andre Christoga <andrechristoga@gmail.com>
2016-04-21 09:18:49 +0700

    Larger 404 text, lead style

--- 404.php
+++ 404.php
@@ -26,8 +26,8 @@
     <?php include 'src/navbar.php'; ?>
     <div class="container">
         <center>
-          <h2>404</h2>
-          <h6>The page you are looking for does not exist.</h6>
+          <h1>404</h1>
+          <h6 class="lead">The page you are looking for does not exist.</h6>
           <button class="btn btn-primary">Go Home</button>
         </center>
         <?php include 'src/footer.php'; ?>
