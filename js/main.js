dced25cfee5cc0c263ad39bbc37995fab9e507b0
Andre Christoga <andrechristoga@gmail.com>
2016-05-05 00:51:31 +0700

    Replace href with pathname

--- js/main.js
+++ js/main.js
@@ -68,5 +68,5 @@ function viewMeme(keyword) {
 
 function memeNav(number) {
   var url = number;
-  window.location.href = window.location.href + '/' + url;
+  window.location.href = window.location.pathname + '/' + url;
 }
