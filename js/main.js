af0ce608353fb2afbd8051d1ef7cabe05a3472ed
Andre Christoga <andrechristoga@gmail.com>
2016-05-05 10:56:40 +0700

    Append .php

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ function viewMeme(keyword) {
 
 function memePage(number) {
   var url = number;
-  window.location.href = window.location.pathname + '/' + url;
+  window.location.href = window.location.pathname + '/' + url + '.php';
 }
 
 function memeNav(number) {
