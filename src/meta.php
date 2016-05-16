3024d4bb7eb160a7c295f6cb99b4e285457f73a0
Andre Christoga <andrechristoga@gmail.com>
2016-05-17 06:37:15 +0700

    Included Semantic-UI

--- src/meta.php
+++ src/meta.php
@@ -1,2 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Semantic-UI -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.8/semantic.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.8/semantic.js" charset="utf-8"></script>
